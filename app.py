e57d341a850a5f72e9033276286df2edbce15022
Pradeep Singh <psingh10021998@gmail.com>
2023-05-13 19:04:18 +0530

    commit changes.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
 
 app = Flask(__name__)
 
 posts = {
     0:{
+        'post_id':0,
         'title':'Hello, World!!',
         'content':'This is my first blog post!'
     }
@@ -11,7 +12,7 @@ posts = {
 
 @app.route('/')
 def home():
-    return "Hello World"
+    return render_template('home.html', posts=posts)
 
 @app.route('/post/<int:post_id>')
 def post(post_id):
@@ -19,6 +20,23 @@ def post(post_id):
     if not post:
         return render_template('404.html', message = f"A post with id {post_id} was not found.")
     return render_template('post.html', post=post)
+
+@app.route('/post/form')
+def form():
+    return render_template('create.html')
+
+@app.route('/post/create', methods=['GET','POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        post_id = len(posts)
+        posts[post_id] = {'id':post_id, 'title':title, 'content':content}
+
+        return redirect(url_for('post', post_id=post_id))
+
+    return render_template('create.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
